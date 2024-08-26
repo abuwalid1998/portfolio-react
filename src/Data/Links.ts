11acbc836c9597d1c6f8d9827bdd6a391d294413
Amjad Khaliliah <76123499+abuwalid1998@users.noreply.github.com>
2024-08-26 13:08:44 +0300

    Updates

--- src/Data/Links.ts
+++
@@ -1,2 +0,0 @@
-export const cvlink = "https://drive.google.com/file/d/1bp5k_Q9HhGpl_ekVuOKwruxEmKuHVQGj/view?usp=sharing";
-export const imageUrl = "src/assets/PERSONALPIC.jpg";
--- src/Data/SocialLinks.ts
+++
@@ -1,6 +0,0 @@
-export const socialLinks = {
-    facebook: "https://facebook.com/your-profile",
-    linkedin: "https://linkedin.com/in/your-profile",
-    instagram: "https://instagram.com/your-profile",
-    github: "https://github.com/your-profile",
-};
