6c55842d76b493c9166ae22c5c7e70da88218a0e
Amjad Khaliliah <76123499+abuwalid1998@users.noreply.github.com>
2024-08-26 13:23:20 +0300

    New Line

--- src/Data/Links.ts
+++ src/Data/Links.ts
@@ -1,2 +1,2 @@
 export const cvlink = "https://drive.google.com/file/d/1bp5k_Q9HhGpl_ekVuOKwruxEmKuHVQGj/view?usp=sharing";
-export const imageUrl = "src/assets/PERSONALPIC.jpg";
+export const imageUrl = "portfolio-react/src/assets/PERSONALPIC.jpg";
